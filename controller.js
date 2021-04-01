d4df74cc3e2b42e4ba98312cc66c8ad368c17121
Mateusz <mateuszgrabiec7@gmail.com>
2021-04-01 15:33:36 +0200

    test2

--- controller.js
+++ controller.js
@@ -55,7 +55,7 @@ class Controller {
 
 	handleRoutes() {
 		this.app.get('/',function(req, res){
-			res.send('Hello world!!!');
+			res.send('Hello world!!!123');
 		});
 		this.app.get('/profile',function(req, res){
 			// res.send('Hello world!!');
